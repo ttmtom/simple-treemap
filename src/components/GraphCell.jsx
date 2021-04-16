e3dcb1594b26bf4c4de100664024901b9443ad9a
Tom <tmtam921@gmail.com>
2021-04-17 01:03:41 +0800

    add tooltip

--- src/components/GraphCell.jsx
+++ src/components/GraphCell.jsx
@@ -1,36 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const GraphCell = (props) => {
     const {fullWidth, data} = props;
 
     return (
-        <div
-            style={{
-                width: `${(data.weight / fullWidth) * 100}%`,
-                display: 'inline-block',
-                background: 'red',
-                height: '100%',
-                borderStyle: 'solid',
-                borderWidth: '1px',
-                boxSizing: 'border-box',
-                overflow: 'hidden',
-                background: data.value >= 0 ? 'rgb(102, 255, 102)' : 'rgb(255, 51, 51)',
-            }}
+        <Tooltip
+            title={
+                <span
+                    style={{
+                        height: '100%',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    {data.name}
+                    <br />
+                    {`${data.value * 100}%`}
+                </span>
+            }
         >
-            <span
+            <div
                 style={{
+                    width: `${(data.weight / fullWidth) * 100}%`,
+                    display: 'inline-block',
+                    background: 'red',
                     height: '100%',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
+                    borderStyle: 'solid',
+                    borderWidth: '1px',
+                    boxSizing: 'border-box',
+                    overflow: 'hidden',
+                    background: data.value >= 0 ? 'rgb(102, 255, 102)' : 'rgb(255, 51, 51)',
+                    userSelect: 'none',
                 }}
             >
-                {data.name}
-                <br />
-                {`${data.value * 100}%`}
-            </span>
-        </div>
+                <span
+                    style={{
+                        height: '100%',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    {data.name}
+                    <br />
+                    {`${data.value * 100}%`}
+                </span>
+            </div>
+        </Tooltip>
     )
 };
 
